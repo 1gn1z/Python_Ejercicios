d562a2b1988fda76b9023060c0f38766863271f1
1gn1z_el_angel <kebor327@gmail.com>
2021-06-28 18:54:38 -0500

    Ejercicio 2. Nota; el texto es el argumento de la funcion print

--- ex002_print.py
+++ ex002_print.py
@@ -1,3 +1,4 @@
 # Ejercicio 2. Exponer el uso básico de la función print.
 
+# El texto (dentro de los parentesis) es el ARGUMENTO de la FUNCION print :D
 print('Ejemplo básico de uso de la función print')
