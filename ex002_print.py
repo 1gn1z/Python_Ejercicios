661858b76c426aaba76b5147fb0a2a47d85ff11a
1gn1z_el_angel <kebor327@gmail.com>
2021-06-28 19:11:06 -0500

    Ejercicio 2. 4o commit. Argumento sep

--- ex002_print.py
+++ ex002_print.py
@@ -7,3 +7,16 @@
 # Con el argumento "end" especificamos con que caracter vamos a terminar la impresión del texto.
 # En este caso, vemos que al poner el salto de linea, al imprimirlo termina la impresion con un salto de linea.
 print('Ejemplo básico de uso de la función print 2', end='\n')
+
+print()
+# Al imprimir asi, se muestra correctamente el texto.
+print('Python', 'esta', 'chido!')
+
+# Vamos a usar el argumento SEP (separator), para indicar que caracter usaremos como separador
+print('Python', 'esta', 'chido!', sep='-')
+# Puede ser cualquier caracter que queramos usar (o necesitemos) como separador:
+print('Python', 'esta', 'chido!', sep='*')
+# Puede ser mas de un caracter :D
+print('Python', 'esta', 'chido!', sep='--------')
+print('Python', 'esta', 'chido!', sep=':')
+
