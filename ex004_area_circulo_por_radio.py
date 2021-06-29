cab60d026dd2b542f50016856b12b7f241498192
1gn1z_el_angel <kebor327@gmail.com>
2021-06-28 20:09:34 -0500

    Ejercicio 4. 4o commit. Código del tutorial anexado

--- ex004_area_circulo_por_radio.py
+++ ex004_area_circulo_por_radio.py
@@ -11,7 +11,14 @@ radio = float(input('Ingrese el radio del círculo: '))
 # Bucamos la formula en internet:
 #El área de un círculo es pi multiplicado por el radio al cuadrado (A = π r²).
 
-area = pi * radio**2
+area = pi * radio ** 2 
 print(f'El área del círculo es: {area}')
 
 # TODO ESTO LO HICE SIN VER EL TUTORIAL Y YA QUEDÓ :31
+
+#   El código del tutorial es practicamente el mismo; lo anexo aqui abajo
+
+#   from math import pi
+#   r = float(input('Escriba el radio del círculo: '))
+#   area = pi * r ** 2
+#   print('El área del círculo es {}'.format(area))
