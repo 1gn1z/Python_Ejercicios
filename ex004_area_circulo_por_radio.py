aaf082a9bb8504630b8ea7ec1ae0489bd2b75bd3
1gn1z_el_angel <kebor327@gmail.com>
2021-06-28 19:59:10 -0500

    Ejercicio 4. 1er commit. importacion de math y pi

---
+++ ex004_area_circulo_por_radio.py
@@ -0,0 +1,9 @@
+# Ejercicio 4: Solicitar el valor del radio de un círculo para calcular su área.
+
+# Vamo' a importar la libreria MATH, especificamente lo que necesitamos, la parte del PI:
+from math import pi
+
+radio = int(input('Ingrese el radio del círculo: '))
+
+
+area = 
