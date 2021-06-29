c20d13cbd1c1f55dcb4ca27ae0d0b81edb7e197c
1gn1z_el_angel <kebor327@gmail.com>
2021-06-28 19:00:31 -0500

    Ejercicio 3. 3er commit. Argumento end

--- ex002_print.py
+++ ex002_print.py
@@ -1,4 +1,9 @@
 # Ejercicio 2. Exponer el uso básico de la función print.
 
 # El texto (dentro de los parentesis) es el ARGUMENTO de la FUNCION print :D
-print('Ejemplo básico de uso de la función print')
+#print('Ejemplo básico de uso de la función print')
+
+
+# Con el argumento "end" especificamos con que caracter vamos a terminar la impresión del texto.
+# En este caso, vemos que al poner el salto de linea, al imprimirlo termina la impresion con un salto de linea.
+print('Ejemplo básico de uso de la función print 2', end='\n')
