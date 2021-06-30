bd25e419ab29efe30ac274c92e4bbd7cb9c6180d
1gn1z_el_angel <kebor327@gmail.com>
2021-06-29 21:48:21 -0500

    Ejercicio 7. 1er commit. division del nombre con split, usando el punto como separador

---
+++ ex007_extensión_archivo.py
@@ -0,0 +1,14 @@
+# Ejercicio 7: Obtener la extensión de un archivo especificado por el usuario.
+
+nombre_archivo = input('Ingrese el nombre del archivo: ')
+
+# Al igual que en el ejercicio 6, usaremos el metodo SPLIT, que nos permite especificar que caracter usaremos como separador.
+
+# main.py
+
+# Tenemos el nombre del archivo y su extension (despues del punto)
+# Sobre otra variable, vamos a guardar el resultado de invocar el metodo split, y le indicamos que el separador
+# va a ser el punto.
+
+partes_nombre_archivo = nombre_archivo.split('.')
+print(partes_nombre_archivo)
