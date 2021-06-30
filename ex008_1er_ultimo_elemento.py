7ee8af5c5c2b3d35b6283587acd97e73ddf4bdf8
1gn1z_el_angel <kebor327@gmail.com>
2021-06-29 22:54:19 -0500

    Ejercicio 8. 2o commit. resuelto con idea del tuto (sin verlo)

--- ex008_1er_ultimo_elemento.py
+++ ex008_1er_ultimo_elemento.py
@@ -4,3 +4,18 @@
 lista = [1, 2, 3, 4, 5]
 print(lista[0::4])
 
+# Siguiendo el tutorial:
+
+lenguajes = ['Python', 'C#', 'PHP', 'Java', 'Javascript']
+
+# Primer elemento: Python
+# Último elemento: javascript
+
+# Dieron la idea de guardar el primer y ultimo elemento en variables:
+
+primer_elemento=lenguajes[0]
+ultimo_elemento=lenguajes[-1]
+
+print(primer_elemento, ultimo_elemento)
+
+# Con esto ya funciona :D
