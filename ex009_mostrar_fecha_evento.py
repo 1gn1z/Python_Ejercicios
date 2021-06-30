c1e7e6d4f9b32846f09df4b114f77ea742a431c1
1gn1z_el_angel <kebor327@gmail.com>
2021-06-29 23:09:40 -0500

    Ejercicio 9. 2o commit. agregue el caracter / para mejor presentación

--- ex009_mostrar_fecha_evento.py
+++ ex009_mostrar_fecha_evento.py
@@ -9,6 +9,6 @@ print('El evento programado será para la fecha:', fecha_evento)
 # Pero sigue mostrandolo tal cual. Vamo a usar unos PLACEHOLDERS:
 
 # Estos PLACEHOLDERS se mapean a cada uno de los elementos que tiene la tupla
-print('El evento programado será para la fecha: %i %i %i' % fecha_evento)    # %i es de INTEGER o entero. En lugar de coma ponemos %
+print('El evento programado será para la fecha: %i/%i/%i' % fecha_evento)    # %i es de INTEGER o entero. En lugar de coma ponemos %
 
 
